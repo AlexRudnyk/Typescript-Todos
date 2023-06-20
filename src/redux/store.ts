e3d94a443476822aabe40b46b45cca8991518ea7
AlexRudnyk <alexander.rudnyk@ukr.net>
2023-06-20 17:34:12 +0300

    adds persist storage

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,11 +1,37 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { todoReducer } from "./todoSlice";
+import storage from "redux-persist/lib/storage";
+import {
+  persistReducer,
+  persistStore,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+
+const persistTasksConfig = {
+  key: "todos",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistTasksConfig, todoReducer);
 
 export const store = configureStore({
   reducer: {
-    todos: todoReducer,
+    todos: persistedReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
